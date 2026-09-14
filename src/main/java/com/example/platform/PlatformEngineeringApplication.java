696f26c370559d4c7d841944ca31799ba28d7ed6
aldo2510 <josealdotrucios@gmail.com>
2026-09-14 18:51:55 -0500

    feat: add Spring Boot application

---
+++ src/main/java/com/example/platform/PlatformEngineeringApplication.java
@@ -0,0 +1,11 @@
+package com.example.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformEngineeringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformEngineeringApplication.class, args);
+    }
+}
